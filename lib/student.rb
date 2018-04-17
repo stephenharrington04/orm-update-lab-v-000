fff9029653cf78654bc5f4a03ff4e05def43caf9
Stephen Harrington <stephenharrington04@yahoo.com>
2018-04-17 15:10:05 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,9 +5,9 @@ class Student
   attr_reader :id
 
   def initialize(id=nil, name, grade)
-    @id = id
     @name = name
     @grade = grade
+    @id = id
   end
 
   def self.create_table
@@ -16,18 +16,14 @@ class Student
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade INTEGER
-      )
+      );
     SQL
 
     DB[:conn].execute(sql)
   end
 
   def self.drop_table
-    sql = <<-SQL
-      DROP TABLE students
-    SQL
-
-    DB[:conn].execute(sql)
+    DB[:conn].execute("DROP TABLE students")
   end
 
   def save
@@ -40,33 +36,25 @@ class Student
       SQL
 
       DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students ")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
 
-  def update
-    sql = <<-SQL
-      UPDATE students SET name = ?, grade = ? WHERE id = ?
-    SQL
-
-    DB[:conn].execute(sql, self.name, self.grade, self.id)
-  end
-
   def self.create(name, grade)
-    fng = Student.new(name, grade)
-    fng.save
-    fng
+    new_student = Student.new(name, grade)
+    new_student.save
+    new_student
   end
 
   def self.new_from_db(row)
-    fng = self.new(row[0], row[1], row[2])
+    new_student = self.new(row[0], row[1], row[2])
   end
 
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE students.name = ?
+      WHERE name = ?
       LIMIT 1
     SQL
 
@@ -75,5 +63,14 @@ class Student
     end.first
   end
 
+  def update
+    sql = <<-SQL
+      UPDATE students
+      SET name = ?, grade = ?
+      WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 end
