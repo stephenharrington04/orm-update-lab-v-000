62fb392fa6862f2aa956d0613d05def6d6b4ab41
Stephen Harrington <stephenharrington04@yahoo.com>
2018-04-17 14:42:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,16 @@
 require_relative "../config/environment.rb"
 
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  
+
 
 end
