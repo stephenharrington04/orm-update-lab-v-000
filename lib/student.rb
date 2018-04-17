6a2eaaaa77cef1e49f4a8203236b434502cd15f2
Stephen Harrington <stephenharrington04@yahoo.com>
2018-04-17 14:43:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,15 @@ class Student
     @id = id
   end
 
-  
+  def self.create_table(name, grade)
+    sql = <<-SQL
+      CREATE TABLE students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      );
+    SQL
+  end
 
 
 end
