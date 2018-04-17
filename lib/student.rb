9c24f3e26900e076236440012bed1da5fc8a55db
Stephen Harrington <stephenharrington04@yahoo.com>
2018-04-17 14:54:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,8 +46,12 @@ class Student
     new_student
   end
 
-  def self.new_from_db
-    
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
 end
