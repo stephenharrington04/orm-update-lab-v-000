5afbc6ca8afa4ab0dd9a44ebe78eb424c4828bb0
Stephen Harrington <stephenharrington04@yahoo.com>
2018-04-17 14:49:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,9 +26,21 @@ class Student
     DB[:conn].execute("DROP TABLE students")
   end
 
-  def save(name, grade)
-    
+  def save
+    if self.idea
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid FROM students")[0][0]
+    end
   end
+
+  
 
 
 end
