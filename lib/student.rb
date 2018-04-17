c42e027936f33433575d8f7be20bb9a0022de2cf
Stephen Harrington <stephenharrington04@yahoo.com>
2018-04-17 15:06:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,8 +47,7 @@ class Student
   end
 
   def self.new_from_db(row)
-    new_student = Student.new(row[0], row[1], row[2])
-    new_student
+    new_student = self.new(row[0], row[1], row[2])
   end
 
   def self.find_by_name(name)
